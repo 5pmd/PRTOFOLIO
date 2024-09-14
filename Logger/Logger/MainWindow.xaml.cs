28f91163d4826feedc6533d43fd83967ee929e4c
Anantha PC <idananth@gmail.com>
2024-09-14 20:03:39 +0700

    Create UI

---
+++ Logger/Logger/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Logger.ViewModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Logger
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary> 
+    public partial class MainWindow : Window
+    {
+        private LoggerViewModel _viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            _viewModel = new LoggerViewModel();
+            DataContext = _viewModel;
+            // Example activity logging
+            _viewModel.LogActivity("Application started.");
+
+            SomeActivityMethod();
+
+            for (int i = 0; i < 20; i++)
+            {
+                _viewModel.LogActivity($"Application started. {i}");
+            }
+        }
+
+        private void SomeActivityMethod()
+        {
+            // Log some activities
+            _viewModel.LogActivity("Some activity performed.");
+        }
+    }
+}
---
+++ Logger/Logger/ViewModel/LoggerViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Logger.Model;
+
+namespace Logger.ViewModel
+{
+    public class LoggerViewModel : INotifyPropertyChanged
+    {
+        private MyLogger _logger;
+
+        public LoggerViewModel()
+        {
+            _logger = new MyLogger();
+        }
+
+        public ObservableCollection<string> LogMessages => _logger.LogMessages;
+
+        public void LogActivity(string activity)
+        {
+            _logger.Log(activity);
+           // OnPropertyChanged(nameof(LogMessages));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
